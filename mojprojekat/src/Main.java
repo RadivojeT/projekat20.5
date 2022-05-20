dcc18386e5a46f729db66864fce83021496bc8d7
Radivoje <rade.tesic92@gmail.com>
2022-05-20 21:40:18 +0200

    Je li proslo ovo?

--- mojprojekat/src/Main.java
+++ mojprojekat/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Zdravo!");
+        System.out.println("Mislim da sam savlado ovo");
+        int a = 2;
+        int b = 3;
+        int c = a+b;
+        System.out.println("Pomozi Bozeee!");
     }
 }
